74503c896c2bc8bdd9402cda1f929191a1a40dfa
tomoya-onishi <tomoya-onishi@x-power.co.jp>
2020-11-09 10:56:04 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="page-title">
+        The creative crew
+      </header>
+      <h1 className="topic">
+        WHO WE ARE
+      </h1>
+      <p className="info">
+        We are team of creatively diverse.  driven.  innovative individuals working in various locations from the world.
+      </p>
+    </div>
+  );
+}
+
+export default App;
